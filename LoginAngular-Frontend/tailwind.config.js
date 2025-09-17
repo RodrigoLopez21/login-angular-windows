ef12efbd022fbfeeedc96c1fa5e1fb7988a124b7
mares1402 <maresd1402@gmail.com>
2025-09-16 21:04:15 -0600

    Actualizacion front-login

--- LoginAngular-Frontend/tailwind.config.js
+++ LoginAngular-Frontend/tailwind.config.js
@@ -3,61 +3,44 @@ module.exports = {
   content: [
     "./src/**/**/*.{html,ts}",
     "./src/**/*.{html,ts}",
-    "./node_modules/flowbite/**/*.js", // add this line
-    "./node_modules/flowbite/**/**/*.js", // add this line
-    
-  ],
-  theme: {
-    extend: {},
-  },
-  
-  plugins: [
-    require('flowbite/plugin')({
-      charts: true,
-  }),
+    "./node_modules/flowbite/**/*.js",
+    "./node_modules/flowbite/**/**/*.js",
   ],
   darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
+        primary: {
+          "50": "#fceef0",   // muy claro
+          "100": "#f8d7dc",
+          "200": "#f1aeb6",
+          "300": "#e97d8a",
+          "400": "#e04c5e",
+          "500": "#c92e41",  // tono medio guindo
+          "600": "#a32435",
+          "700": "#801b2a",
+          "800": "#5c121f",
+          "900": "#3a0a14",  // muy oscuro
+          "950": "#24060b"
+        }
+      },
+      fontFamily: {
+        body: [
+          'Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'Segoe UI',
+          'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif',
+          'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'
+        ],
+        sans: [
+          'Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'Segoe UI',
+          'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif',
+          'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'
+        ]
       }
-    },
-    fontFamily: {
-      'body': [
-    'Inter', 
-    'ui-sans-serif', 
-    'system-ui', 
-    '-apple-system', 
-    'system-ui', 
-    'Segoe UI', 
-    'Roboto', 
-    'Helvetica Neue', 
-    'Arial', 
-    'Noto Sans', 
-    'sans-serif', 
-    'Apple Color Emoji', 
-    'Segoe UI Emoji', 
-    'Segoe UI Symbol', 
-    'Noto Color Emoji'
-  ],
-      'sans': [
-    'Inter', 
-    'ui-sans-serif', 
-    'system-ui', 
-    '-apple-system', 
-    'system-ui', 
-    'Segoe UI', 
-    'Roboto', 
-    'Helvetica Neue', 
-    'Arial', 
-    'Noto Sans', 
-    'sans-serif', 
-    'Apple Color Emoji', 
-    'Segoe UI Emoji', 
-    'Segoe UI Symbol', 
-    'Noto Color Emoji'
-  ]
     }
-  }
+  },
+  plugins: [
+    require('flowbite/plugin')({
+      charts: true,
+    }),
+  ],
 }
